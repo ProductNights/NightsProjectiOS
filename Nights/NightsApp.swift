6e2f28f689d07903f2bb93f0ff976a2be54bc586
Nights Product <product@nights.com.es>
2024-07-15 17:53:33 +0200

    Initial Commit

---
+++ Nights/NightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightsApp.swift
+//  Nights
+//
+//  Created by Marc Puig on 15/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
